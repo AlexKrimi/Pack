5f804cc098d242b9380592e56a49331a287d13bf
Alex Krimi <kriminetz0810@gmail.com>
2018-06-24 12:23:14 -0700

    networking fixed

---
+++ App/PidManager/PidManager.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 6/24/18.
+//
+
+#ifndef PACK_PIDMANAGER_H
+#define PACK_PIDMANAGER_H
+
+#endif //PACK_PIDMANAGER_H
---
+++ Container.h
@@ -0,0 +1,8 @@
+//
+// Created by alex on 6/24/18.
+//
+
+#ifndef PACK_CONTAINER_H
+#define PACK_CONTAINER_H
+
+#endif //PACK_CONTAINER_H
